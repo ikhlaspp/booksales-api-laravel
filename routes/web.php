d78696db75f74e830a03296b57ce83ad37340fd2
ikhlaspp <ikhlasputra940@gmail.com>
2026-04-21 09:48:47 +0700

    tugas laravel 1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\AuthorController;
+
+Route::get('/genres', [GenreController::class, 'index']);
+Route::get('/authors', [AuthorController::class, 'index']);
+
