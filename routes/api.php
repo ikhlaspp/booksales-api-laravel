53ce45338a8dafcd15cf1bf49e5d17cb3647a827
ikhlaspp <ikhlasputra940@gmail.com>
2026-04-27 22:25:38 +0700

    tugas 6_middleware

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,21 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthController;
+use App\Http\Middleware\Admin;
+
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('genres', GenreController::class);
-Route::apiResource('authors', AuthorController::class);
-Route::apiResource('books', BookController::class);
+Route::apiResource('genres', GenreController::class)->only(['index', 'show']);
+Route::apiResource('authors', AuthorController::class)->only(['index', 'show']);
+Route::apiResource('books', BookController::class)->only(['index', 'show']);
+
+Route::middleware(['auth:sanctum', Admin::class])->group(function () {
+    Route::apiResource('genres', GenreController::class)->except(['index', 'show']);
+    Route::apiResource('authors', AuthorController::class)->except(['index', 'show']);
+    Route::apiResource('books', BookController::class)->except(['index', 'show']);
+});
